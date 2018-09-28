2422fecf390efefe20f16541b4e0f02d82603fd6
KentPhan <kent.chi.lam@gmail.com>
2018-09-28 11:26:10 -0600

    Had to readd logging to project. added levels

--- KPEngine/include/Utils/KP_Log.h
+++ KPEngine/include/Utils/KP_Log.h
@@ -5,7 +5,24 @@ namespace KPEngine
 {
 	namespace Utils
 	{
-		void KP_Log(const char * i_fmt)
+		enum LogType
+		{
+			NoLogging = 0,
+			Fatal,
+			Error,
+			Warning,
+			Display,
+			Log,
+			Verbose,
+			VeryVerbose,
+			All = VeryVerbose,
+			NumVerbosity,
+			VerbosityMask = 0xf,
+			SetColor = 0x40,
+			BreakOnLog = 0x80,
+		};
+
+		void KP_Log(const char * i_fmt, ...)
 		{
 			
 		}
