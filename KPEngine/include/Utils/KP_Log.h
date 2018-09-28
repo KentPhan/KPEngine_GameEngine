4556416bf2b0f7099410d9bcb187b5ce41c9bc83
KentPhan <u0734504@ad.utah.edu>
2018-09-28 15:33:36 -0600

    Added some debug code

--- KPEngine/include/Utils/KP_Log.h
+++ KPEngine/include/Utils/KP_Log.h
@@ -22,20 +22,20 @@ namespace KPEngine
 			BreakOnLog = 0x80,
 		};
 
-		void KP_Log(const char * i_fmt, ...)
+		void KP_Log(LogType type, const char * i_fmt, ...)
 		{
 			
 		}
 
-		//// This is how we handle variable argument pre-processor macros
-		//// This is only supported by compilers that support the C99 standard
-		//#if defined(_DEBUG)  &&  !defined(DISABLE_DEBUG_PRINT)
-		//#define DEBUG_PRINT(fmt,...) KP_Log((fmt),__VA_ARGS__)
-		//#else
-		//// This is one way we create a do nothing (NO OP) macro that doesn't
-		//// generate a compiler warning or error
-		//#define DEBUG_PRINT(fmt,...) void(0)
-		//#endif
+		// This is how we handle variable argument pre-processor macros
+		// This is only supported by compilers that support the C99 standard
+		#if defined(_DEBUG)  &&  !defined(DISABLE_DEBUG_PRINT)
+		#define DEBUG_PRINT(type,fmt,...) KP_Log((type),(fmt),__VA_ARGS__)
+		#else
+		// This is one way we create a do nothing (NO OP) macro that doesn't
+		// generate a compiler warning or error
+		#define DEBUG_PRINT(type,fmt,...) void(0)
+		#endif
 
 	}
 }
