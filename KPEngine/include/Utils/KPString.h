9ea9cc9da6e27b79f390dcc6d54c78ac60ff07c9
KentPhan <kent.chi.lam@gmail.com>
2018-09-28 23:45:52 -0600

    Attempted to enhance my own string class

--- KPEngine/include/Utils/KPString.h
+++ KPEngine/include/Utils/KPString.h
@@ -7,11 +7,20 @@ namespace KPEngine
 		class KPString
 		{
 		public:
+
+			// Constructors Destructors
 			KPString();
 			KPString(const char * i_str);
 			~KPString();
+
+			// Accessors
 			char* Get();
 			int length();
+
+			//operators
+			KPString& operator+(const KPString & i_other);
+
+
 		private:
 			char* m_str;
 			int m_length;
--- KPEngine/src/Utils/KPString.cpp
+++ KPEngine/src/Utils/KPString.cpp
@@ -5,11 +5,11 @@ namespace KPEngine
 {
 	namespace Utils
 	{
+		// Constructors Destructors
 		KPString::KPString()
 		{
 			m_str = nullptr;
 		}
-
 		KPString::KPString(const char* i_str)
 		{
 
@@ -34,13 +34,14 @@ namespace KPEngine
 			m_str[m_length] = '\0';
 
 		}
-
-
+		// TODO Define Copy Constructor
 		KPString::~KPString()
 		{
 			delete m_str;
 		}
 
+
+		// Accessors
 		char* KPString::Get()
 		{
 			return m_str;
@@ -49,6 +50,43 @@ namespace KPEngine
 		int KPString::length()
 		{
 			return m_length;
+		}
+
+
+		// Operators
+		// TODO: This does not work and is still a work in progress
+		KPString& KPString::operator+(const KPString& i_other)
+		{
+			// new length
+			int newLength = this->m_length + i_other.m_length;
+
+			// concat new string
+			char* newString = new char[newLength + 1];
+
+			/*char* left = this->m_str;
+			char* right = i_other.m_str;
+
+			for (int i = 0; i < newLength; i++)
+			{
+				
+				if(left )
+				newString[i] = *(start);
+				start++;
+			}
+			newString[newLength] = '\0';*/
+
+			strcat_s(newString, (newLength + 1), this->m_str);
+			strcat_s(newString, (newLength + 1), i_other.m_str);
+
+
+			// remove old one
+			delete this->m_str;
+
+			// replace with new one
+			this->m_str = newString;
+
+			
+			return *this;
 		}
 	}
 	
