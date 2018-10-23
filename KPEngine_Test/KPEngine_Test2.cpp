f86ab78b5fb57f38185e03a959340e2bf4c20f0c
KentPhan <u0734504@ad.utah.edu>
2018-10-22 20:04:59 -0600

    Regenerating file to make it non binary

---
+++ KPEngine_Test/KPEngine_Test2.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <iostream>
+#include "../KPEngine/include/KPEngine-init.h"
+
+extern bool HeapManager_UnitTest();
+
+int main()
+{
+
+	/*KPEngine::Initialize();*/
+
+	HeapManager_UnitTest();
+
+	std::getchar();
+}
