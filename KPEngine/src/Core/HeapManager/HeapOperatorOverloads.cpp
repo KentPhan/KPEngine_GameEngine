521ecc14c04499764ff672c955d4894ea109d838
KentPhan <kent.chi.lam@gmail.com>
2018-12-10 16:25:45 -0700

    Swapped in with my own Heap Manager

--- KPEngine/src/Core/HeapManager/HeapOperatorOverloads.cpp
+++ KPEngine/src/Core/HeapManager/HeapOperatorOverloads.cpp
@@ -4,22 +4,36 @@
 #include "../../../include/Core/HeapManager/KPHeapManager.h"
 #include <cinttypes>
 #include <malloc.h>
+#include <Windows.h>
+#include <cassert>
+
+KPEngine::Core::HeapManager::KPHeapManager* pUnderlyingHeap;
 
 void * operator new(size_t i_size)
 {
 	DEBUG_PRINT(KPEngine::Utils::KPLogType::Verbose, "Calling new( size_t ) with ( %zx ).\n", i_size);
 
-	//KPEngine::Core::HeapManager::KPHeapManager::create()
+	if(!pUnderlyingHeap)
+	{
+		const size_t sizeHeap = 1024 * 1024;
+		void * pHeapMemory = HeapAlloc(GetProcessHeap(), 0, sizeHeap);
+		pUnderlyingHeap = KPEngine::Core::HeapManager::KPHeapManager::create(pHeapMemory, sizeHeap);
+	}
 
-	return _aligned_malloc(i_size, 4 );
+	
+	return pUnderlyingHeap->_alloc(i_size);
 }
 
 void operator delete(void* i_ptr)
 {
+	assert(pUnderlyingHeap);
 	DEBUG_PRINT(KPEngine::Utils::KPLogType::Verbose, "Calling delete( void* ) with ( %" PRIxPTR" ).\n", i_ptr);
 
 	if (i_ptr)
-		_aligned_free(i_ptr);
+	{
+		pUnderlyingHeap->_free(i_ptr);
+	}
+		
 }
 
 
