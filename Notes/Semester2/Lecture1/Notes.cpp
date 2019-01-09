b931de6049e4281a0e73b294cda5c340514b793f
KentPhan <u0734504@ad.utah.edu>
2019-01-08 17:55:48 -0700

    Moved Notes

---
+++ Notes/Semester2/Lecture1/Notes.cpp
@@ -0,0 +1,32 @@
+Notes:
+- Structures:
+\Engine\GameCommon
+\Engine\EngineHigh
+\Engine\EngineLow\FileIO
+\Engine\Core\Containers\BitArray.h
+\Engine\Core\Containers\BitArray.cpp
+
+
+
+Questions:
+
+Lecture:
+- GameObject interface ?
+- In moving objects around and detecting collisions. i did something where I checked if something would collided before moving it. Your
+slides imply that i move it in the reverse direction if a collision happens?
+
+
+MyStuff
+- Go over my Final
+- I tried to organize my code and ended up having extra pathings. How do I improve this? 
+
+Unreal
+- We re trying to make a game in pure c++ unreal engine
+- Renaming/Refactoring classes in unreal engine. Fairly difficult I found.
+- Generated file at the top of unreal engine c++.
+- Quickly go over what we re doing in our structure and if that sounds right.
+- ECS and unreal EC. How does unreal engine handle multiprocessor ussage in comparison to Unity's new system
+
+Class
+- Confirm Game Engine Architecture book. Yas. Got it
+- Canvas Page?
