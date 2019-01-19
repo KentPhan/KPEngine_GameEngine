9515c64cf985be44ca7f0b4b757e7ccb618ea5de
KentPhan <u0734504@ad.utah.edu>
2019-01-19 14:02:12 -0700

    Fixed Configurations for GLib

--- KPEngine/src/KPEngine-init.cpp
+++ KPEngine/src/KPEngine-init.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <iostream>
 #include "../include/Utils/KP_Log.h"
+#include "../include/Core/HeapManager/MemorySystem.h"
 //#include "../include/Core/HeapManager/MemorySystem.h"
 //#include "../include/Core/HeapManager/KPMixedHeapAllocators.h"
 
@@ -15,9 +16,9 @@ namespace KPEngine
 	bool Initialize()
 	{
 		// Custom Heap Manager and Memory System
-		/*const size_t	sizeHeap = 1024 * 1024;
+		const size_t	sizeHeap = 1024 * 1024;
 		KPEngine::pHeapMemory = HeapAlloc(GetProcessHeap(), 0, sizeHeap);
-		KPEngine::Core::HeapManager::MemorySystem::InitializeMemorySystem(pHeapMemory, sizeHeap);*/
+		KPEngine::Core::HeapManager::MemorySystem::InitializeMemorySystem(pHeapMemory, sizeHeap);
 
 		DEBUG_PRINT(KPLogType::Verbose ,"Engine Initialized");
 		std::cout << "KPEngine Initialized\n";
@@ -29,8 +30,8 @@ namespace KPEngine
 	{
 
 		// Clean up your Memory System (HeapManager and FixedSizeAllocators)
-		/*KPEngine::Core::HeapManager::MemorySystem::DestroyMemorySystem();
-		HeapFree(GetProcessHeap(), 0, pHeapMemory);*/
+		KPEngine::Core::HeapManager::MemorySystem::DestroyMemorySystem();
+		HeapFree(GetProcessHeap(), 0, pHeapMemory);
 
 		DEBUG_PRINT(KPLogType::Verbose, "Engine Cleaned Up");
 		std::cout << "KPEngine Cleaned Up\n";
