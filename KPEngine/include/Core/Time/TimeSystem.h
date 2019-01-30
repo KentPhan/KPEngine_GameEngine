144fb356fb778f8a77ef295afd64e190712ccb50
KentPhan <kent.chi.lam@gmail.com>
2019-01-30 16:01:35 -0700

    Added Time System Files

---
+++ KPEngine/include/Core/Time/TimeSystem.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+
+namespace KPEngine
+{
+	namespace Core
+	{
+		namespace Time
+		{
+			class TimeSystem
+			{
+			public:
+				TimeSystem();
+				~TimeSystem();
+			};
+		}
+	}
+}
+
+
+
+
---
+++ KPEngine/src/Core/Time/TimeSystem.cpp
@@ -0,0 +1,20 @@
+
+#include "../../../include/Core/Time/TimeSystem.h"
+
+namespace KPEngine
+{
+	namespace Core
+	{
+		namespace Time
+		{
+			TimeSystem::TimeSystem()
+			{
+			}
+
+
+			TimeSystem::~TimeSystem()
+			{
+			}
+		}
+	}
+}
