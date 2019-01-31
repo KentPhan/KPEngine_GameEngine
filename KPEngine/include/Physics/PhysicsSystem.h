747b70f3a25eaffbafcfac05f8592c72f4b19a46
KentPhan <kent.chi.lam@gmail.com>
2019-01-30 19:26:24 -0700

    added Physics Step

--- KPEngine/include/Physics/PhysicsSystem.h
+++ KPEngine/include/Physics/PhysicsSystem.h
@@ -1,13 +1,23 @@
 #pragma once
+#include <vector>
+
 namespace KPEngine
 {
 	namespace Physics
 	{
+		class PhysicsComponent;
+
 		class PhysicsSystem
 		{
 		public:
 			PhysicsSystem();
+
+			void PhysicsStep();
+
 			~PhysicsSystem();
+
+		private:
+			std::vector<PhysicsComponent*> m_PhysicsComponents;
 		};
 	}
 }
