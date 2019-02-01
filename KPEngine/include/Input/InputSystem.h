bbd3c73f792083ab4d80c9cc7150afd9af212ba1
KentPhan <kent.chi.lam@gmail.com>
2019-02-01 02:28:04 -0700

    Updated Project References. Added InputSystem File

---
+++ KPEngine/include/Input/InputSystem.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace KPEngine
+{
+	namespace Input
+	{
+		class InputSystem
+		{
+		public:
+			InputSystem();
+			~InputSystem();
+		};
+
+	}
+}
---
+++ KPEngine/src/Input/InputSystem.cpp
@@ -0,0 +1,18 @@
+
+#include "../../include/Input/InputSystem.h"
+
+namespace KPEngine
+{
+	namespace Input
+	{
+		InputSystem::InputSystem()
+		{
+		}
+
+
+		InputSystem::~InputSystem()
+		{
+		}
+
+	}
+}
