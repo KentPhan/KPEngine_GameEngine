24c6aeac28d3da0316af7d976342c73b48b04cbd
KentPhan <kent.chi.lam@gmail.com>
2019-03-28 00:20:41 -0600

    Added Matrix Files

---
+++ KPEngine/include/Utils/KPMatrix4x4.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+namespace KPEngine
+{
+	namespace Utils
+	{
+		class KPMatrix4x4
+		{
+		public:
+			KPMatrix4x4();
+			~KPMatrix4x4();
+		};
+	}
+}
+
+
---
+++ KPEngine/src/Utils/KPMatrix4x4.cpp
@@ -0,0 +1,11 @@
+#include "../../include/Utils/KPMatrix4x4.h"
+
+
+KPEngine::Utils::KPMatrix4x4::KPMatrix4x4()
+{
+}
+
+
+KPEngine::Utils::KPMatrix4x4::~KPMatrix4x4()
+{
+}
