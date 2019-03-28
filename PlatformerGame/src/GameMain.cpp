cbbe43f42297bfaf20ac28f8825492cb7446720f
KentPhan <kent.chi.lam@gmail.com>
2019-03-28 00:06:52 -0600

    Trying to track the friggin memory leak.

--- PlatformerGame/src/GameMain.cpp
+++ PlatformerGame/src/GameMain.cpp
@@ -2,23 +2,24 @@
 // PlatformerGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 
+
+
 //#include "KPEngine-init.h"
 #include <iostream>
 #include <Windows.h>
-
-
-#include <assert.h>
-
-#include <stdint.h>
-
 
 #include "GLib.h"
 #include "KPEngine-init.h"
 #include "../include/PlatformerGame.h"
 
+#define CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
 
 int WINAPI wWinMain(HINSTANCE i_hInstance, HINSTANCE i_hPrevInstance, LPWSTR i_lpCmdLine, int i_nCmdShow)
 {
+	_CrtSetBreakAlloc(589);
 	/*_CrtSetBreakAlloc(591);
 	_CrtSetBreakAlloc(593);*/
 	//_CrtSetBreakAlloc(241);
--- PlatformerGame/src/PlatformerGame.cpp
+++ PlatformerGame/src/PlatformerGame.cpp
@@ -1,6 +1,3 @@
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
 
 #include <iostream>
 #include "conio.h"
