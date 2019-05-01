e168f3b068a840511ed8babb317ac5759c5e6d80
KentPhan <kent.chi.lam@gmail.com>
2019-05-01 02:08:07 -0600

    Started on Code for Messagring System

---
+++ KPEngine/src/Messaging/MessagingSystem.cpp
@@ -0,0 +1,11 @@
+#include "../../include/Messaging/MessagingSystem.h"
+
+
+KPEngine::Messaging::MessagingSystem::MessagingSystem()
+{
+}
+
+
+KPEngine::Messaging::MessagingSystem::~MessagingSystem()
+{
+}
